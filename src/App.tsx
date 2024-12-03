43a8e673f78559993c6d9e82ea7fdfb754532ffd
Andy Mina <andy.mina@mongodb.com>
2024-12-02 21:11:18 -0500

    extract framedata

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,58 @@
 import './App.css';
 import { useRef, useState, useEffect, useCallback } from 'react';
 
+function computeBacklightFrame(frame?: ImageData) {
+  if (!frame) return;
+  console.log(frame);
+}
+
 function App() {
   const [_, setDummyState] = useState(false);
   const videoRef = useRef<HTMLVideoElement>(null);
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null); // only to get the current video frame
+  const contextRef = useRef<CanvasRenderingContext2D | null>();
 
-  const ctx = canvasRef.current?.getContext('2d');
+  const handleFrame = useCallback(() => {
+    // grab the current frame
+    contextRef.current?.drawImage(
+      videoRef.current!,
+      0,
+      0,
+      window.innerWidth,
+      window.innerHeight
+    );
+    const frameData = contextRef.current?.getImageData(
+      0,
+      0,
+      window.innerWidth,
+      window.innerHeight
+    );
 
-  const rerender = useCallback(() => {
-    setDummyState((prevState) => !prevState);
-    videoRef.current?.requestVideoFrameCallback(rerender);
+    // handle fancy logic here
+    const backlightFrame = computeBacklightFrame(frameData);
+    // draw the backlight frame onto the context
+
+    setDummyState((prev) => !prev); // hack to force rerender
+    videoRef.current?.requestVideoFrameCallback(handleFrame);
   }, []);
 
   // runs once when the component first renders
   useEffect(
     function setup() {
+      // set the context
+      contextRef.current = canvasRef.current?.getContext('2d', {
+        willReadFrequently: true,
+      });
       videoRef.current?.addEventListener('play', () =>
-        videoRef.current?.requestVideoFrameCallback(rerender)
+        videoRef.current?.requestVideoFrameCallback(handleFrame)
       );
     },
-    [rerender]
+    [handleFrame]
   );
-
-  if (videoRef.current) {
-    ctx?.drawImage(videoRef.current, 0, 0);
-  }
 
   return (
     <div className='main'>
       <canvas
-        id='canvas'
         ref={canvasRef}
         width={window.innerWidth}
         height={window.innerHeight}
