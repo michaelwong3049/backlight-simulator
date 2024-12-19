395c1b69cf16e450b725926bdacf6b78e74386b0
Andy Mina <andy.mina@mongodb.com>
2024-12-18 23:56:10 -0500

    restructure and remove magic numbers

--- src/utils/colorManipulation.ts
+++ src/utils/colorManipulation.ts
@@ -1,4 +1,9 @@
 import { Dimensions, BacklightOptions, Division } from '@/types';
+import {
+  BLUE_CHANNEL_OFFSET,
+  GREEN_CHANNEL_OFFSET,
+  RED_CHANNEL_OFFSET,
+} from '@/utils/constants';
 
 export function computeBacklightFrame(
   // TODO: remove ctx later, we should be able to do some assignments on frame.data
@@ -15,8 +20,8 @@ export function computeBacklightFrame(
     false
   );
 
-  divisions.forEach((division, idx) => {
-    ctx.fillStyle = `rgb(${division.color[0]}, ${division.color[1]}, ${division.color[2]})`;
+  divisions.forEach((division) => {
+    ctx.fillStyle = `rgb(${division.color[RED_CHANNEL_OFFSET]}, ${division.color[GREEN_CHANNEL_OFFSET]}, ${division.color[BLUE_CHANNEL_OFFSET]})`;
     ctx.fillRect(division.col, division.row, division.width, division.height);
   });
 
@@ -75,9 +80,9 @@ export function computeDivisions(
         )
       );
       if (debugColoring) {
-        color[0] = isFirstFit ? 255 : 0;
-        color[1] = isLastFit ? 255 : 0;
-        color[2] = rowAboveVideo && colAboveVideo ? 255 : 0;
+        color[RED_CHANNEL_OFFSET] = isFirstFit ? 255 : 0;
+        color[GREEN_CHANNEL_OFFSET] = isLastFit ? 255 : 0;
+        color[BLUE_CHANNEL_OFFSET] = rowAboveVideo && colAboveVideo ? 255 : 0;
       }
 
       divisions[divisionIdx++] = {
@@ -104,103 +109,108 @@ export function getAverageColor(
   const res = [0, 0, 0, 255];
   for (let row = startRow; row < endRow; row++) {
     for (let col = startCol; col < endCol; col++) {
-      const p = getPixel(frame, row, col);
+      const pixelIdx = (row * frame.width + col) * 4;
 
-      res[0] += p[0];
-      res[1] += p[1];
-      res[2] += p[2];
+      res[RED_CHANNEL_OFFSET] += frame.data[pixelIdx + RED_CHANNEL_OFFSET];
+      res[GREEN_CHANNEL_OFFSET] += frame.data[pixelIdx + GREEN_CHANNEL_OFFSET];
+      res[BLUE_CHANNEL_OFFSET] += frame.data[pixelIdx + BLUE_CHANNEL_OFFSET];
     }
   }
   const numPixels = (endRow - startRow) * (endCol - startCol);
-  res[0] /= numPixels;
-  res[1] /= numPixels;
-  res[2] /= numPixels;
+  res[RED_CHANNEL_OFFSET] /= numPixels;
+  res[GREEN_CHANNEL_OFFSET] /= numPixels;
+  res[BLUE_CHANNEL_OFFSET] /= numPixels;
 
   return res;
 }
-
-// TODO: convenience method, but if we're worried about perf we can remove and just direct index access
-function getPixel(frame: ImageData, row: number, col: number) {
-  // TODO: could be a better index OOB handling
-  if (row < 0 || row >= frame.height || col < 0 || col >= frame.width) {
-    console.error(
-      `Index out of bounds error during pixel access in ImageData! Attempted to get pixel at (row: ${row}, col: ${col})`
-    );
-    return EMPTY_PIXEL;
-  }
-
-  const truePixelIdx = (row * frame.width + col) * 4;
-  return frame.data.slice(truePixelIdx, truePixelIdx + 4);
-}
-
-const EMPTY_PIXEL = [0, 0, 0, 255];
 
 /**
  * - A function that is used to smooth the color transition between divisions
  * @param frame is the default canvas that holds the divison width/length colors]
  * - Defines the boundaries for region convolution
- * @param startRow 
- * @param startCol 
- * @param endRow 
- * @param endCol 
+ * @param startRow
+ * @param startCol
+ * @param endRow
+ * @param endCol
  * @param kernel_size is how large your kernel convolution size is (higher = laggier but smoother transition)
- * @returns an ImageData 
+ * @returns an ImageData
  */
 
 export function regionConvolution(
-    frame: ImageData,
-    startRow: number,
-    startCol: number,
-    endRow: number,
-    endCol: number,
-    kernel_size: number
+  frame: ImageData,
+  startRow: number,
+  startCol: number,
+  endRow: number,
+  endCol: number,
+  kernel_size: number
 ): ImageData {
-    const frameCopy = new Uint8ClampedArray(frame.data.length);
-    let layers = Math.floor(kernel_size / 2);
-    for(let row = startRow; row < endRow; row++) { 
-        for(let col = startCol; col < endCol; col++) {
-            let currentPixel = frame.data[(row * frame.width + col) * 4];
+  const frameCopy = new Uint8ClampedArray(frame.data.length);
+  let layers = Math.floor(kernel_size / 2);
+  for (let row = startRow; row < endRow; row++) {
+    for (let col = startCol; col < endCol; col++) {
+      let currentPixel = frame.data[(row * frame.width + col) * 4];
 
-            const result = convolveRegion(row, col, layers, frame, startRow, startCol, endRow, endCol);
+      const result = convolveRegion(
+        row,
+        col,
+        layers,
+        frame,
+        startRow,
+        startCol,
+        endRow,
+        endCol
+      );
 
-            frameCopy[(currentPixel++)] = result[0] / kernel_size * kernel_size;
-            frameCopy[(currentPixel++)] = result[1] / kernel_size * kernel_size;
-            frameCopy[(currentPixel++)] = result[2] / kernel_size * kernel_size;
-            frameCopy[(currentPixel++)] = result[3] / kernel_size * kernel_size;
-        }
+      frameCopy[currentPixel++] = (result[0] / kernel_size) * kernel_size;
+      frameCopy[currentPixel++] = (result[1] / kernel_size) * kernel_size;
+      frameCopy[currentPixel++] = (result[2] / kernel_size) * kernel_size;
+      frameCopy[currentPixel++] = (result[3] / kernel_size) * kernel_size;
     }
+  }
 
-    return new ImageData(frameCopy, (endRow - startRow), (endCol - startCol))
+  return new ImageData(frameCopy, endRow - startRow, endCol - startCol);
 }
 
 export function convolveRegion(
-    row: number,
-    col: number, 
-    layers: number, 
-    frame: ImageData,
-    startRow: number,
-    startCol: number,
-    endRow: number,
-    endCol: number
+  row: number,
+  col: number,
+  layers: number,
+  frame: ImageData,
+  startRow: number,
+  startCol: number,
+  endRow: number,
+  endCol: number
 ) {
-    let red = 0, blue = 0, green = 0, alpha = 255;
-    for(let kernel_row = row-layers; kernel_row < row+layers+1; kernel_row++) { 
-        for(let kernel_col = col-layers; kernel_col < col+layers+1; kernel_col++) {
-            const rowOutOfBounds =  row - layers < startRow || row + layers >= endRow;
-            const columnOutOfBounds = col - layers < startCol || col + layers >= endCol;
-            if(rowOutOfBounds || columnOutOfBounds){
-                continue;
-            }
+  let red = 0,
+    blue = 0,
+    green = 0,
+    alpha = 255;
+  for (
+    let kernel_row = row - layers;
+    kernel_row < row + layers + 1;
+    kernel_row++
+  ) {
+    for (
+      let kernel_col = col - layers;
+      kernel_col < col + layers + 1;
+      kernel_col++
+    ) {
+      const rowOutOfBounds = row - layers < startRow || row + layers >= endRow;
+      const columnOutOfBounds =
+        col - layers < startCol || col + layers >= endCol;
+      if (rowOutOfBounds || columnOutOfBounds) {
+        continue;
+      }
 
-            let currentKernelPixel = frame.data[(kernel_row * (frame.width) + kernel_col) * 4];
+      let currentKernelPixel =
+        frame.data[(kernel_row * frame.width + kernel_col) * 4];
 
-            red += frame.data[currentKernelPixel++];
-            green += frame.data[currentKernelPixel++];
-            blue += frame.data[currentKernelPixel++];
-            alpha += frame.data[currentKernelPixel++];
-        }
+      red += frame.data[currentKernelPixel++];
+      green += frame.data[currentKernelPixel++];
+      blue += frame.data[currentKernelPixel++];
+      alpha += frame.data[currentKernelPixel++];
     }
-    
-    return [red, green, blue, alpha]
-}
+  }
 
+  return [red, green, blue, alpha];
+}
