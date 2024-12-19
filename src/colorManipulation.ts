61f7b9ad6c9df12d87d89d178147ecf77e5ab43e
michaelwong3049 <michaelwong3049@gmail.com>
2024-12-18 19:16:44 -0500

    cleanup code

--- src/colorManipulation.ts
+++ src/colorManipulation.ts
@@ -156,54 +156,51 @@ export function regionConvolution(
     kernel_size: number
 ): ImageData {
     const frameCopy = new Uint8ClampedArray(frame.data.length);
+    let layers = Math.floor(kernel_size / 2);
     for(let row = startRow; row < endRow; row++) { 
         for(let col = startCol; col < endCol; col++) {
             let currentPixel = frame.data[(row * frame.width + col) * 4];
-            let red = 0, blue = 0, green = 0, alpha = 255;
-            let layers = Math.floor(kernel_size / 2);
 
-            convoluteRegion(row, col, layers, frame, endRow, startRow, red, green, blue, alpha);
+            const result = convolveRegion(row, col, layers, frame, startRow, startCol, endRow, endCol);
 
-            frameCopy[(currentPixel++)] = red / kernel_size * kernel_size;
-            frameCopy[(currentPixel++)] = green / kernel_size * kernel_size;
-            frameCopy[(currentPixel++)] = blue / kernel_size * kernel_size;
-            frameCopy[(currentPixel++)] = alpha / kernel_size * kernel_size;
+            frameCopy[(currentPixel++)] = result[0] / kernel_size * kernel_size;
+            frameCopy[(currentPixel++)] = result[1] / kernel_size * kernel_size;
+            frameCopy[(currentPixel++)] = result[2] / kernel_size * kernel_size;
+            frameCopy[(currentPixel++)] = result[3] / kernel_size * kernel_size;
         }
     }
 
     return new ImageData(frameCopy, (endRow - startRow), (endCol - startCol))
 }
 
-function convoluteRegion(
+export function convolveRegion(
     row: number,
     col: number, 
     layers: number, 
     frame: ImageData,
-    endRow: number, 
     startRow: number,
-    red: number,
-    green: number,
-    blue: number,
-    alpha: number
+    startCol: number,
+    endRow: number,
+    endCol: number
 ) {
+    let red = 0, blue = 0, green = 0, alpha = 255;
     for(let kernel_row = row-layers; kernel_row < row+layers+1; kernel_row++) { 
         for(let kernel_col = col-layers; kernel_col < col+layers+1; kernel_col++) {
-            let currentKernelPixel = frame.data[(kernel_row * (frame.width) + kernel_col) * 4];
-            // checking if we are out of the top bounds
-            if(row - (layers) <= 0){
+            const rowOutOfBounds =  row - layers < startRow || row + layers >= endRow;
+            const columnOutOfBounds = col - layers < startCol || col + layers >= endCol;
+            if(rowOutOfBounds || columnOutOfBounds){
                 continue;
             }
 
-            // checking if we are out of the bottom bounds
-            if(row + (layers) >= (endRow - startRow)) {
-                continue;
-            }
-            
-            red += frame.data[currentKernelPixel++]
-            green += frame.data[currentKernelPixel++]
-            blue += frame.data[currentKernelPixel++]
-            alpha += frame.data[currentKernelPixel++]
+            let currentKernelPixel = frame.data[(kernel_row * (frame.width) + kernel_col) * 4];
+
+            red += frame.data[currentKernelPixel++];
+            green += frame.data[currentKernelPixel++];
+            blue += frame.data[currentKernelPixel++];
+            alpha += frame.data[currentKernelPixel++];
         }
     }
+    
+    return [red, green, blue, alpha]
 }
 
